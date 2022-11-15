440c2ff127a11723142bbd40f738413971d2358d
Zach <hoschar.zach@gmail.com>
2022-11-15 17:51:49 -0600

    storing rooms in temp object while server is running, added event listeners to get list of rooms and create one

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { createServer } from "http";
+import { nanoid } from "nanoid";
 import { Server } from "socket.io";
 
 const app = express();
+
+
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
@@ -11,8 +14,33 @@ const io = new Server(httpServer, {
     }
 });
 
+
+let rooms = []
 io.on("connection", (socket) => {
-    console.log('connection')
+
+    socket.on("create room", (name, callback) => {
+        console.log("NAME", name)
+        const roomId = nanoid()
+        const newRoom = {
+            roomId,
+            name
+        }
+        rooms.push(newRoom)
+        callback(newRoom)
+    })
+
+    socket.on('get rooms', () => {
+        io.emit(rooms)
+    })
+
+    socket.on("get rooms", (callback) => {
+        callback(rooms);
+    });
+
+    socket.on('messageAll', (data) => {
+        console.log(data)
+        socket.emit(data)
+    })
 });
 
 httpServer.listen(3000);
